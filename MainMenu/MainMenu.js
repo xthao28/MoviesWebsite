d7fe49d3bce123fc6c8834ba67eb1754123dbced
nguyendung087 <93911127+nguyendung087@users.noreply.github.com>
2022-11-28 15:56:13 +0700

    Add files via upload

---
+++ MainMenu/MainMenu.js
@@ -0,0 +1,15 @@
+
+// Film Lists
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
